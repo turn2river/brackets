6d4225e10b7033f4b3c219466c8a3d61a10cbed7
alexey.krylov <turn2river@gmail.com>
2022-02-24 22:32:29 +0300

    feat: implement check brackets func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  str = str.split('');
+  const stack = [];
+  const bracketsObjCheck = {};
+  bracketsConfig.forEach((e) => {
+    bracketsObjCheck[e[1]] = e[0];
+  });
+  for (let i = 0; i < str.length; i++) {
+    if (stack.length === 0) {
+      stack.push(str[i]);
+    } else if (stack[stack.length - 1] === bracketsObjCheck[str[i]]) {
+      stack.pop();
+    } else {
+      stack.push(str[i]);
+    }
+  }
+  return stack.length ? false : true;
+};
